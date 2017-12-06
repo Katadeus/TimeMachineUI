89f4b512ab726612356e11fe38d4f9baeacafa28
Axel <stewaa5@rpi.edu>
2017-12-06 15:47:51 -0500

    Added entry blast explosion slider & visual.

---
+++ Time Machine UI/Assets/ExplosionScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExplosionScaler : MonoBehaviour {
+
+    public Slider slider;
+    public RectTransform rectTransform;
+
+	// Use this for initialization
+	void Start () {
+        rectTransform = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slider.value * 8);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slider.value * 8);
+	}
+}
